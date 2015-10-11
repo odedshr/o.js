7ea28b26f7881fa1b592c29944563f21bf5bb592
Oded Sharon <odedsh@gmail.com>
2015-10-11 22:17:10 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,13 @@
+〇=(function(){
+    this.EVT.subscribe('window.onload',(function refreshAndStartObserver() {
+        〇.ELM.per('.js-version').html(this.VER.framework);
+        var count = 1;
+        this.ELM.per('h1,h2,h3,h4').each((function perHeader(dElm) {
+            var name = dElm.innerHTML.replace(/\W/g,'')+(count++);
+            var data = {anchor:name,title:dElm.innerHTML,className: dElm.tagName.toLowerCase()};
+            dElm.innerHTML = this.TPL.render({'headerTemplate':data});
+            this.DOM.append(this.ELM.navigation,this.TPL.render({'navLinkTemplate':data}));
+        }).bind(this));
+    }).bind(this));
+
+return this;}).call((typeof 〇 !== 'undefined')?〇:{});
---
+++ o.unit.js
@@ -0,0 +1,265 @@
+〇=(function(){
+    QUnit.module( '〇.EVT' );
+    QUnit.test( 'subscribing listeners and dispatching events', function( assert ) {
+        (function testSubcribeAndDispatch () {
+            var done = assert.async();
+            var handler = function subscribed(){
+                assert.ok(true,'Event subscribed and dispatched');
+                clearTimeout(onTimeOut);
+                done();
+            };
+            var onTimeOut = setTimeout(function() {
+                assert.ok( false, 'Event wasn\'t dispatched' );
+                done();
+            });
+            〇.EVT.subscribe('anEvent',handler);
+            〇.EVT.dispatch('anEvent');
+            〇.EVT.unsubscribe('anEvent',handler);
+        })();
+        (function testUnsubscribe () {
+            var done = assert.async();
+            var handler = function subscribed(){
+                assert.ok(false,'event wasn\'t unsubscribed');
+                clearTimeout(onTimeOut);
+                done();
+            };
+            var onTimeOut = setTimeout(function() {
+                assert.ok( true, 'Event was unsubscribed' );
+                done();
+            });
+            〇.EVT.subscribe('anEvent',handler);
+            〇.EVT.unsubscribe('anEvent',handler);
+            〇.EVT.dispatch('anEvent');
+        })();
+        (function testPassParameter () {
+            var done = assert.async();
+            var parameter = 'parameter';
+            var handler = function subscribed(evt){
+                assert.ok(evt.detail === parameter,'Passed a parameter');
+                clearTimeout(onTimeOut);
+                done();
+            };
+            var onTimeOut = setTimeout(function() {
+                assert.ok( false, 'Event wasn\'t dispatched' );
+                done();
+            });
+            〇.EVT.subscribe('anEvent',handler);
+            〇.EVT.dispatch('anEvent',parameter);
+            〇.EVT.unsubscribe('anEvent',handler);
+        })();
+        (function testGetDispatcher () {
+            var done = assert.async();
+            var parameter = 'parameter';
+            var handler = function subscribed(evt){
+                assert.ok(evt.detail === parameter,'Sent parameter via dispatcher');
+                clearTimeout(onTimeOut);
+                done();
+            };
+            var onTimeOut = setTimeout(function() {
+                assert.ok( false, 'Event wasn\'t dispatched' );
+                done();
+            });
+            〇.EVT.subscribe('anEvent',handler);
+            var dispatcher = 〇.EVT.getDispatcher('anEvent');
+            dispatcher(parameter);
+            〇.EVT.unsubscribe('anEvent',handler);
+        })();
+    });
+    QUnit.module( '〇.CSS' );
+    QUnit.test( 'Manipulation of CSS styling', function( assert ) {
+        var element = 〇.DOM.create('span', { 'id': 'cssTestObject'},'myCSS');
+        var addedCss = 'test-o-css';
+        〇.DOM.append(document.body, element);
+        〇.CSS.add(element,addedCss);
+        assert.ok( element.className.split(' ').indexOf('js-'+addedCss) > -1 , 'Add a style' );
+        assert.ok( 〇.CSS.has(element,addedCss), 'Has a style' );
+        〇.CSS.remove(element,addedCss);
+        assert.ok( element.className.split(' ').indexOf('js-'+addedCss) === -1, 'Remove a style' );
+        assert.ok( !〇.CSS.has(element,addedCss), 'Doesn\'t Has a style' );
+        〇.CSS.toggle(element,addedCss);
+        assert.ok( element.className.split(' ').indexOf('js-'+addedCss) > - 1, 'Toggle a style on' );
+        〇.CSS.toggle(element,addedCss);
+        assert.ok( element.className.split(' ').indexOf('js-'+addedCss) === -1, 'Toggle a style off' );
+        〇.DOM.remove(element);
+        assert.ok( element.className.split(' ').indexOf('js-'+addedCss) === -1, 'Toggle a style off' );
+        〇.CSS.isJsPrefixed(false);
+        assert.equal( 〇.CSS.isJsPrefixed(), false, 'jsPrefixed is false' );
+        〇.CSS.isJsPrefixed(true);
+        assert.equal( 〇.CSS.isJsPrefixed(), true, 'jsPrefixed is true' );
+    });
+    QUnit.module( '〇.ELM' );
+    QUnit.test( "Finding DOM Elements easily", function( assert ) {
+        var running = false;
+        var getElement = function getElement (id) {
+            return 〇.DOM.create('span', { id: id, class: 'elm-test-unit'},'amazing');
+        };
+        //function cannot run while testAddAndRemoveElement is running so it will be trigger by it
+        var testDisableObserver = function testDisableObserver () {
+            var done = assert.async();
+            running = 'disableObserver';
+            〇.ELM.observe(false);
+            〇.DOM.append(document.body, getElement('disableObserver'));
+            window.setTimeout(function afterAdding() {
+                assert.ok( 〇.ELM['disableObserver'] !== document.getElementById('disableObserver'), 'Added DOM element but ELM list not updated when observer disconnected' );
+                〇.ELM.observe(true);
+                assert.equal( 〇.ELM['disableObserver'], document.getElementById('disableObserver'), 'ELM list updated after observer re-enabled' );
+                〇.DOM.remove(〇.ELM['disableObserver']);
+                running = false;
+                done();
+                testMultiElementManipulation();
+            },1);
+        };
+        // since function contain many related asserts it will run only after all other tests
+        var testMultiElementManipulation = function testMultiElementManipulation () {
+            var done = assert.async();
+            〇.DOM.append(document.body, getElement('perElement1'));
+            〇.DOM.append(document.body, getElement('perElement2'));
+            var elms = 〇.ELM.per ('.elm-test-unit');
+            assert.equal( elms.length, 2, '2 elements selected using per' );
+            assert.equal( elms[0].getAttribute('id'), 'perElement1', 'Random-access first element' );
+            elms.css.add('test-add-class');
+            assert.equal( document.querySelectorAll('.js-test-add-class').length, 2, 'style added to 2 elements' );
+            elms.css.remove('test-add-class');
+            assert.equal( document.querySelectorAll('.js-test-add-class').length, 0, 'Style removed from 2 elements' );
+            elms.css.toggle('test-toggle-class');
+            assert.equal( document.querySelectorAll('.js-test-toggle-class').length, 2, 'Style toggle for 2 elements' );
+            var count = 0;
+            elms.each(function (elm) { count += ( 〇.CSS.has(elm, 'test-toggle-class') ? 1 : 0); });
+            assert.equal( count, 2, 'Counted EACH element' );
+            elms.remove();
+            assert.equal( document.querySelectorAll('.elm-test-unit').length, 0, '2 items removed' );
+            done();
+        };
+
+        (function testFindElement () {
+            var done = assert.async();
+            assert.equal( 〇.ELM.qunit, document.getElementById('qunit'), "Existing DOM element found" );
+            done();
+        })();
+
+        (function testAddAndRemoveElement () {
+            var done = assert.async();
+            running = 'addAndRemoveAElement';
+            〇.DOM.append(document.body, getElement('addAndRemoveAElement'));
+            window.setTimeout(function afterAdding() {
+                assert.equal( 〇.ELM['addAndRemoveAElement'], document.getElementById('addAndRemoveAElement'), "Added DOM element found" );
+                〇.DOM.remove(〇.ELM['addAndRemoveAElement']);
+                window.setTimeout ( function afterRemoving() {
+                    assert.equal( typeof 〇.ELM['addAndRemoveAElement'], 'undefined', "Removed DOM element" );
+                    running = false;
+                    done();
+                    testDisableObserver();
+                },1);
+            },1);
+        })();
+    });
+    QUnit.module( '〇.DOM' );
+    QUnit.test( 'Manipulation of DOM elements', function( assert ) {
+        var getElement = function getElement (id) {
+            return 〇.DOM.create('div', { id: id, class: 'dom-test-unit'});
+        };
+        var baseElement = 〇.DOM.parse('<div id="domTests">dom-test-unit</div>');
+        〇.DOM.append(〇.ELM['qunit-fixture'],baseElement);
+
+        〇.DOM.prepend(〇.ELM['qunit-fixture'],getElement('prependTest'));
+        〇.DOM.append(〇.ELM['qunit-fixture'],getElement('appendTest'));
+        〇.DOM.insertBefore(baseElement,getElement('insertBeforeTest'));
+        〇.DOM.insertAfter(baseElement,getElement('insertAfterTest'));
+
+        var elements = baseElement.parentNode.childNodes;
+        assert.equal( elements[0].getAttribute('id'),  'prependTest', 'Prepend an element' );
+        assert.equal( elements[1].getAttribute('id'),  'insertBeforeTest', 'InsertBefore an element' );
+        assert.equal( elements[3].getAttribute('id'),  'insertAfterTest', 'InsertAfter an element' );
+        assert.equal( elements[4].getAttribute('id'),  'appendTest', 'Append an element' );
+
+        〇.DOM.remove(baseElement);
+        assert.equal( document.getElementById('domTests'), null, 'Remove an element' );
+    });
+    QUnit.module( '〇.AJAX' );
+    QUnit.test( 'Sending & Receiving data via AJAX', function( assert ) {
+        var baseURL = 'https://httpbin.org/';
+        (function testAjaxGet () {
+            var done = assert.async();
+            〇.AJAX.get(baseURL+'get', function (response) {
+                assert.equal( response.url, baseURL+'get', "got JSON response for GET request" );
+                done();
+            });
+        })();
+        (function testAjaxPost () {
+            var done = assert.async();
+            var data = {myKey : 'myValue'};
+            〇.AJAX.post (baseURL+'post', data, function (response) {
+                assert.equal( JSON.parse(response.data).myKey, data.myKey, "got JSON response for POST request" );
+                done();
+            });
+        })();
+        (function testAjaxPut () {
+            var done = assert.async();
+            var data = {myKey : 'myValue'};
+            〇.AJAX.put( baseURL+'put', data, function (response) {
+                assert.equal( JSON.parse(response.data).myKey, data.myKey, "got JSON response for PUT request" );
+                done();
+            });
+        })();
+        (function testAjaxDelete () {
+            var done = assert.async();
+            〇.AJAX.delete( baseURL+'delete', function (response) {
+                assert.equal( response.url,baseURL+'delete', "got JSON response for DELETE request" );
+                done();
+            });
+        })();
+        (function testAjaxWithNewDefaults () {
+            var done = assert.async();
+            〇.AJAX.setDefaults({
+               type: false,
+               url : baseURL+'get',
+               callback: function (response) {
+                   assert.equal( JSON.parse(response).url, baseURL+'get', "Request with defaults set can work without parameters" );
+               }
+            });
+            〇.AJAX.get();
+            〇.AJAX.setDefaults({ url: false, callback: false});
+            try {
+                〇.AJAX.get();
+            }
+            catch (error) {
+                assert.equal( error.message,'missing-url', "Request without URL throws an exception" );
+            }
+            done();
+        })();
+    });
+    QUnit.module( '〇.TPL' );
+    QUnit.test( 'Rendering HTML using templates', function( assert ) {
+        〇.TPL.load('o.templates.html');
+        assert.equal( 〇.TPL.render({'helloWorldTemplate':{name:'World'}}),' <div>Hello World</div>', 'Render a simple helloWorld' );
+        assert.equal( 〇.TPL.render({'nestedDataTemplate':{user:{name:'John',age:27}}}),' <div>User:John<br/>Age:27</div>', 'Render nested data' );
+        assert.equal( 〇.TPL.render({'arrayTemplate':{users:{user:[{name:'John'},{name:'Peter'}]}}}),' <ul><li>0 John</li><li>1 Peter</li></ul>', 'Render data array' );
+        assert.equal( 〇.TPL.render({'helloWorldTemplate':{name:function(){return 'Function';}}}),' <div>Hello Function</div>', 'Render a template using function' );
+        assert.equal( 〇.TPL.render({'templateWithSubTemplate':{user:{name:'World'}}}), ' <div>includes helloWorld:  <div>Hello name</div></div>', 'render sub-template' );
+        assert.equal( 〇.TPL.render({'ifTemplate':{isShown:true}}),' <div>Shown!</div>', 'Render template with conditional' );
+        assert.equal( 〇.TPL.render({'innerDataTemplate':{user:{name:'John',age:27}}}),' <div>User:name = John <br/> Age = 27 </div>', 'Render nested data' );
+        〇.TPL.appendToLanguage('es_sp',{'label.Hello':'Hola!'}).setLocale('es_sp');
+        assert.equal( 〇.TPL.render({'translatedWorldTemplate':{name:'World'}}),' <div>Hola! World</div>', 'Add word to language-strings, setLocale and render HelloWorld' );
+        〇.TPL.setLocale(false);
+        assert.equal( 〇.TPL.render({'translatedWorldTemplate':{name:'World'}}),' <div>Hello World</div>', 'SetLocale to default and render HelloWorld' );
+        assert.equal( 〇.TPL.render({'changeTagsTemplate':{name:'World'}}),' <div>Hello World World</div>', 'Update delimiters' );
+        // testLoadTemplate is disabled due to bug in chrome (apparently)
+        /*(function testLoadLanguage () {
+            var done = assert.async();
+            〇.EVT.subscribe('TPL.languageLoaded',function (evt) {
+                assert.equal (evt.detail[0],'de-de', 'Language loaded from external file');
+                done();
+            });
+            〇.TPL.loadLanguage('de-de.json');
+        })();*/
+        // testLoadTemplate is disabled due to bug in chrome (apparently)
+        /*(function testLoadTemplate () {
+            var done = assert.async();
+            〇.EVT.subscribe('TPL.loaded',function (evt) {
+                assert.ok (evt.detail.indexOf('loadedTemplate') > -1, 'Template loaded from external file');
+                done();
+            });
+            〇.TPL.load('o.templates.html');
+        })();*/
+    });
+return this;}).call((typeof 〇 != "undefined")?〇:{});
